5eb10b57be5019ca9df98936979bf4f548b950e3
poojaprabhuswamy <pooja.prabhuswamy@gmail.com>
2016-12-03 12:45:51 -0800

    Localization update.

--- HTML/assets/js/localization.js
+++ HTML/assets/js/localization.js
@@ -12,63 +12,27 @@ $.get("http://ipinfo.io", function (response) {
       console.log("IP:",ip,loc,resp,loc);
       
       if (loc == "ES") {
-      $(location).attr('href', './locale/index_sp.html')
+      $(location).attr('href', './index_sp.html')
       }
       
       if (loc == "CN") {
-      $(location).attr('href', './locale/index_cn.html')
+      $(location).attr('href', './index_cn.html')
       }
       
       if (loc == "IN") {
-      $(location).attr('href', './locale/index_hi.html')
+      $(location).attr('href', './index_hi.html')
       }
       
       if (loc == "IL") {
-      $(location).attr('href', './locale/index_hb.html')
+      $(location).attr('href', './index_hb.html')
       }
       
       if (loc == "JP") {
-      $(location).attr('href', './locale/index_jp.html')
+      $(location).attr('href', './index_jp.html')
       }
       
       }, "jsonp");
 
 
 
-    function renderContactPage_CN() {
-        if(login == "1")
-            location.href = "./locale/contact_cn.html?login=1&userid="+user;
-        else
-            location.href = "./locale/contact_cn.html?login=0";
-    }
-    function renderContactPage_HB() {
-        if(login == "1")
-            location.href = "./locale/contact_hb.html?login=1&userid="+user;
-        else
-            location.href = "./locale/contact_hb.html?login=0";
-    }
-    function renderContactPage_JP() {
-        if(login == "1")
-            location.href = "./locale/contact_jp.html?login=1&userid="+user;
-        else
-            location.href = "./locale/contact_jp.html?login=0";
-    }
-
-    function renderAboutPage_CN() {
-        if(login == "1")
-            location.href = "./locale/pages-about-us_cn.html?login=1&userid="+user;
-        else
-            location.href = "./locale/pages-about-us_cn.html?login=0";
-    }
-    function renderAboutPage_HB() {
-        if(login == "1")
-            location.href = "./locale/pages-about-us_hb.html?login=1&userid="+user;
-        else
-            location.href = "./locale/pages-about-us_hb.html?login=0";
-    }
-    function renderAboutPage_JP() {
-        if(login == "1")
-            location.href = "./locale/pages-about-us_jp.html?login=1&userid="+user;
-        else
-            location.href = "./locale/pages-about-us_jp.html?login=0";
-    }
+    
