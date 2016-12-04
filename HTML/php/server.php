1f82be3b1a051abab38ba874e4520fc5af487024
Usha Vyas <ushapraveena.b@gmail.com>
2016-12-04 03:02:40 -0800

    Sign up hopefully fix

--- HTML/php/server.php
+++ HTML/php/server.php
@@ -1,33 +1,32 @@
 <?php
-	    header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: POST');
         header('Access-Control-Max-Age: 1000');
         header("content-type: application/json");
-        
-	$jsonobj = new stdClass();
+
+        $jsonobj = new stdClass();
         $jsonobj->operation = null;
 
-	$conn = new mysqli('eclectic.ciwss8n8nsgm.us-west-1.rds.amazonaws.com', 'cmpe280', 'cmpe280project', 'eclectic');
-  // Check connection
+        $conn = mysqli_connect('eclectic.ciwss8n8nsgm.us-west-1.rds.amazonaws.com', 'cmpe280', 'cmpe280project', 'eclectic');
+        // Check connection
   if ($conn->connect_error >0) {
     $jsonobj->operation = "Error" ;
     die("Connection failed: " . $conn->connect_error);
-  } 
+  }
 // through the global variable $_POST, like this:
-  $fname = isset($_REQUEST['fname']) ? $_REQUEST['fname'] : 'null';
-  $lname = isset($_REQUEST['lname']) ? $_REQUEST['lname'] : 'null';
-  $email = isset($_REQUEST['email1']) ? $_REQUEST['email1'] : 'null';
-  $password = isset($_REQUEST['password1']) ? $_REQUEST['password1'] : 'null';
+  $fname= $_POST['fname'];
+  $lname= $_POST['lname'];
+  $email1= $_POST['email1'];
+  $password1=$_POST['password1'];
 
-  $sql = "INSERT INTO registration(FIRSTNAME,LASTNAME,EMAIL,PASSWORD) VALUES('".$fname."','".$lname."','".$email."','".$password."');";
-
-  if($conn->query($sql) == TRUE){
-      //print("error <br>");
-      $jsonobj->sqlINSERTstatus = 'Success';
-      $jsonobj->userid = $email;
-  }else{
+  $sql = "INSERT INTO registration(firstname,lastname,email,password) VALUES('".$fname."','".$lname."','".$email1."','".$password1."');";
+  if($conn->query($sql) === TRUE){
+      $jsonobj->sqlINSERTstatus = 'success';
+      $jsonobj->userid = $fname;
+  } else {
       $jsonobj->sqlINSERTstatus = 'DUPLICATEError';
   }
   $jsonobj->operation = "success";
+  $conn->close();
   echo json_encode($jsonobj);
 ?>
